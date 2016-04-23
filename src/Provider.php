98b1b7fa33fd009e8f07a5f18b1d33a9e855448c
bingooo <hiweibin@gmail.com>
2016-04-23 23:59:39 +0800

    Update Provider.php

--- src/Provider.php
+++ src/Provider.php
@@ -21,7 +21,7 @@ class Provider extends AbstractProvider implements ProviderInterface
     /**
      * {@inheritdoc}.
      */
-    protected $scopes = ['snsapi_login'];
+    protected $scopes = ['snsapi_userinfo'];
 
     /**
      * set Open Id.
